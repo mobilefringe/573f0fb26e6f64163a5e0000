5a964dd5d1a4b0e5c3c27b82edf92096b98b40f0
shinda@mobilefringe.com @ CodeCloud <shinda@mobilefringe.com @ CodeCloud>
2016-05-20 18:41:53 -0400

    Las Vegas Dining Passport 1.1  /locale/en.js

---
+++ locale/en.js
@@ -0,0 +1,13 @@
+var __n = {
+      "modalEnterCodeTitle" : "Enter Today's Code",
+      "modalEnterCodeMessage" : "You can request today's code from the front.",
+      "modalEnterCodeBusy" : "Verifying Today's Code",
+      'btnActionSignIn' : 'Sign In',
+      'btnActionCreateAccount' : 'Create Account',
+      'btnActionCancel' : 'Cancel',
+      'modalLoginTitle' : 'Sign In',
+      'modalAllCheckedInTitle' : 'Congratulations!' , 
+      'modalAllCheckedInBody' : 'Thanks for checking in at all our locations! Stop by Guest SErvices to redeem your prize.<br/><br/>Remember to Sign In to keep your check-ins saved in your account!',
+      'modalCheckinFailBody': "The code you tried doesn't seem to be valid. Double check your code and try agian.",
+        'modalCheckinFailTitle': "Whoops!"
+};
