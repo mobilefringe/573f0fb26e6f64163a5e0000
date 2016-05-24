19bc0ab0ef2290e93c914ad1f7dabb6fe02755b0
shinda@mobilefringe.com @ CodeCloud <shinda@mobilefringe.com @ CodeCloud>
2016-05-24 04:13:11 -0400

    Las Vegas Dining Passport 1.1  /sign-up.js

--- sign-up.js
+++ sign-up.js
@@ -1,7 +1,13 @@
 myApp.onPageInit('sign-up', function(page){
 
-  function onFormSubmit(e) {
+  function onFormSubmit(errors, e) {
     e.preventDefault();
+
+    if (errors.length > 0) {
+      // Display an alert with all our errors
+      myApp.alert(errors[0].messages.join("<br/>"), __n['modalRegistrationErrorsTitle']);
+      return false;
+    }
 
     function onSuccess(result) {      
       myApp.hidePreloader();
@@ -30,7 +36,32 @@ myApp.onPageInit('sign-up', function(page){
     return false;
   }
 
+  var validator = new FormValidator('sign-up-form', 
+    [
+      {
+        name: 'name',
+        display: 'Name',
+        rules: 'required'
+      },
+      {
+        name: 'email',
+        display: 'Email',
+        rules: 'required|valid_email'
+      },
+      {
+        name: 'password',
+        display: 'Password',
+        rules: 'required|min_length[4]'
+      },
+      {
+        name: 'password_confirmation',
+        display: 'Password Confirmation',
+        rules: 'required|matches[password]'
+      }
+    ],
+    onFormSubmit);
+
   // Perform the following actions on this page.
-  $('#sign-up form').on('submit', onFormSubmit);
+  // $('#sign-up form').on('submit', onFormSubmit);
 
 });
