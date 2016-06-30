096d231ed71d314a0b6936501cb45a10c11d5231
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-06-30 15:22:45 -0400

    Las Vegas Dining Passport 1.1  /custom-modals.js

--- custom-modals.js
+++ custom-modals.js
@@ -1,21 +1,21 @@
 var failed_login = function() {
-                    myApp.modal({
-                        title:  'Vertical Buttons Layout',
-                        text: 'Vivamus feugiat diam velit. Maecenas aliquet egestas lacus, eget pretium massa mattis non. Donec volutpat euismod nisl in posuere. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae',
-                        verticalButtons: true,
-                        buttons: [
-                            {
-                                text: 'Button 1',
-                                onClick: function() {
-                                    myApp.alert('You clicked first button!')
-                                }
-                            },
-                            {
-                                text: 'Button 2',
-                                onClick: function() {
-                                    myApp.alert('You clicked second button!')
-                                }
-                            }
-                        ]
-                    })
+    myApp.modal({
+        title:  'Vertical Buttons Layout',
+        text: 'Vivamus feugiat diam velit. Maecenas aliquet egestas lacus, eget pretium massa mattis non. Donec volutpat euismod nisl in posuere. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae',
+        verticalButtons: true,
+        buttons: [
+            {
+                text: 'Button 1',
+                onClick: function() {
+                    myApp.alert('You clicked first button!')
                 }
+            },
+            {
+                text: 'Button 2',
+                onClick: function() {
+                    myApp.alert('You clicked second button!')
+                }
+            }
+        ]
+    })
+}
