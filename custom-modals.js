ca901897ed9cc51f9829efbe6c45d7404345c622
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-06-30 15:19:06 -0400

    Las Vegas Dining Passport 1.1  /custom-modals.js

---
+++ custom-modals.js
@@ -0,0 +1,19 @@
+var failed_login = myApp.modal({
+                    title:  'Vertical Buttons Layout',
+                    text: 'Vivamus feugiat diam velit. Maecenas aliquet egestas lacus, eget pretium massa mattis non. Donec volutpat euismod nisl in posuere. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae',
+                    verticalButtons: true,
+                    buttons: [
+                        {
+                            text: 'Button 1',
+                            onClick: function() {
+                                myApp.alert('You clicked first button!')
+                            }
+                        },
+                        {
+                            text: 'Button 2',
+                            onClick: function() {
+                                myApp.alert('You clicked second button!')
+                            }
+                        }
+                    ]
+                })
