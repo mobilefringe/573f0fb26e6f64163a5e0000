3e3046ddcb121525c9832e544cdcba7eb149165a
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-06-30 15:27:04 -0400

    Las Vegas Dining Passport 1.1  /custom-modals.js

--- custom-modals.js
+++ custom-modals.js
@@ -1,6 +1,6 @@
 var failedLogin = function() {
     myApp.modal({
-        title:  'Vertical Buttons Layout',
+        title:  __n['modalCheckinFailTitle'],
         text: 'Vivamus feugiat diam velit. Maecenas aliquet egestas lacus, eget pretium massa mattis non. Donec volutpat euismod nisl in posuere. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae',
         verticalButtons: true,
         buttons: [
