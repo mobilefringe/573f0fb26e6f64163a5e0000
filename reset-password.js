77219b4c92f6bbdbc63fd2ae6da45d8b8edb6752
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-07-07 16:31:16 -0400

    Las Vegas Dining Passport 1.1  /reset-password.js

--- reset-password.js
+++ reset-password.js
@@ -1,3 +1,16 @@
+// Initialize app
+var myApp = new Framework7({
+    modalUsernamePlaceholder: __n['modalUsername']
+});
+ 
+// If we need to use custom DOM library, let's save it to $$ variable:
+var $ = Dom7;
+ 
+// Add view
+var mainView = myApp.addView('.view-main', {
+  // Because we want to use dynamic navbar, we need to enable it for this view:
+  dynamicNavbar: true
+});
 myApp.onPageInit('reset-password', function(page){
 
   function onFormSubmit(errors, e) {
