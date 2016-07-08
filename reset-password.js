84d9ee53b836f28a7eabc72084a8f81887f59494
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-07-08 13:05:57 -0400

    Las Vegas Dining Passport 1.1  /reset-password.js

--- reset-password.js
+++ reset-password.js
@@ -71,7 +71,7 @@ function resetSuccess(result) {
 function resetError(result) {
     var el = "#error_box";
     $(el).html(__n['resetError'])
-    $('#reset-password-form').trigger(reset)
+    $('#reset-password-form').trigger('reset')
     
 }
 
