af6c2a07a15a237d75bfa08b7a0f56de4479ed4b
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-07-08 11:14:04 -0400

    Las Vegas Dining Passport 1.1  /reset-password.js

--- reset-password.js
+++ reset-password.js
@@ -21,7 +21,7 @@ function onFormSubmit(errors, e) {
         }
         var lp = result['loyalty_programs'][programName];
         var submitUrl = lp.details.endpoints.update_password;
-        console.log(submitUrl)
+        submitForm
     }, 'json');
 }
 
@@ -44,3 +44,18 @@ $(document).ready(function(){
   // $('#sign-up form').on('submit', onFormSubmit);
 
 })
+
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
