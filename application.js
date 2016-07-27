9bf73ccb7a6a6d4d0d7bf6dda33b824d2de3de30
tristan@mobilefringe.com @ CodeCloud <tristan@mobilefringe.com @ CodeCloud>
2016-07-27 16:04:37 -0400

    Las Vegas Dining Passport 1.1  /application.js

--- application.js
+++ application.js
@@ -265,10 +265,10 @@ function getUserToken() {
     myApp.showPreloader(__n['modalSignInBusy']);
 
     $.ajax({
+      dataType: 'json',
       url: host + apis['sign-in'],
       data: {username: username, password: password, device_token: deviceToken},
       method: 'POST',
-      dataType: 'json',
       success: onSignInSuccess,
       error: onSignInError
     });
